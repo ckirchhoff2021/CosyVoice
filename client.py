7b41de3ebd8ee04aee5a71b20cf8949c6952bb37
ckirchhoff <ckirchhoff2019@gmail.com>
2025-11-07 11:06:55 +0800

    Create client.py

---
+++ client.py
@@ -0,0 +1,108 @@
+import argparse
+import logging
+import requests
+import torch
+import torchaudio
+import numpy as np
+
+
+def inference_speech():
+    url = "http://{}:{}/inference_{}".format(args.host, args.port, args.mode)
+    if args.mode == 'sft':
+        payload = {
+            'tts_text': args.tts_text,
+            'spk_id': args.spk_id
+        }
+        response = requests.request("GET", url, data=payload, stream=True)
+    elif args.mode == 'zero_shot':
+        payload = {
+            'tts_text': args.tts_text,
+            'prompt_text': args.prompt_text
+        }
+        files = [('prompt_wav', ('prompt_wav', open(args.prompt_wav, 'rb'), 'application/octet-stream'))]
+        response = requests.request("GET", url, data=payload, files=files, stream=True)
+    elif args.mode == 'cross_lingual':
+        payload = {
+            'tts_text': args.tts_text,
+        }
+        files = [('prompt_wav', ('prompt_wav', open(args.prompt_wav, 'rb'), 'application/octet-stream'))]
+        response = requests.request("GET", url, data=payload, files=files, stream=True)
+    else:
+        payload = {
+            'tts_text': args.tts_text,
+            'spk_id': args.spk_id,
+            'instruct_text': args.instruct_text
+        }
+        response = requests.request("GET", url, data=payload, stream=True)
+    tts_audio = b''
+    for r in response.iter_content(chunk_size=16000):
+        tts_audio += r
+    tts_speech = torch.from_numpy(np.array(np.frombuffer(tts_audio, dtype=np.int16))).unsqueeze(dim=0)
+    logging.info('save response to {}'.format(args.tts_wav))
+    torchaudio.save(args.tts_wav, tts_speech, target_sr)
+    logging.info('get response')
+
+
+def openai_speech():
+    url = "http://{}:{}/v1/audio/speech".format(args.host, args.port)
+    payload = {
+        'input': args.tts_text,
+        'voice': args.voice
+    }
+    response = requests.request("POST", url, data=payload, stream=True)
+    tts_audio = b''
+    for r in response.iter_content(chunk_size=16000):
+        tts_audio += r
+    tts_speech = torch.from_numpy(np.array(np.frombuffer(tts_audio, dtype=np.int16))).unsqueeze(dim=0)
+    logging.info('save response to {}'.format(args.tts_wav))
+    torchaudio.save(args.tts_wav, tts_speech, target_sr)
+    logging.info('get response')
+
+
+def main():
+    if args.mode == 'openai':
+        openai_speech()
+    else:
+        inference_speech()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--host',
+                        type=str,
+                        default='0.0.0.0')
+    parser.add_argument('--port',
+                        type=int,
+                        default='50000')
+    parser.add_argument('--mode',
+                        default='zero_shot',
+                        choices=['sft', 'zero_shot', 'cross_lingual', 'instruct', 'openai'],
+                        help='request mode')
+    parser.add_argument('--voice',
+                        type=str,
+                        default='wise')
+    parser.add_argument('--tts_text',
+                        type=str,
+                        default='你好，我是通义千问语音合成大模型，请问有什么可以帮您的吗？')
+    parser.add_argument('--spk_id',
+                        type=str,
+                        default='中文女')
+    parser.add_argument('--prompt_text',
+                        type=str,
+                        default='希望你以后能够做的比我还好呦。')
+    parser.add_argument('--prompt_wav',
+                        type=str,
+                        default='asset/zero_shot_prompt.wav')
+    parser.add_argument('--instruct_text',
+                        type=str,
+                        default='Theo \'Crimson\', is a fiery, passionate rebel leader. \
+                                 Fights with fervor for justice, but struggles with impulsiveness.')
+    parser.add_argument('--tts_wav',
+                        type=str,
+                        default='demo.wav')
+    args = parser.parse_args()
+    prompt_sr, target_sr = 16000, 22050
+    main()
+
+# python client.py  --mode zero_shot --prompt_text 我其实呢不是很喜欢用豆包，而且我不太喜欢用电动车。嗯，我只喜欢油车，我是一个非常聪明的人。 --prompt_wav /home/chenxiang.101/workspace/Self.wav --tts_text "很高兴认识你，我是爱新觉罗玄烨。"
+# python client.py --mode openai --tts_text 很高兴认识你，我是爱新觉罗玄烨。 --tts_wav outputs/xxx.wav
